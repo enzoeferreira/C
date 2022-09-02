53eec66d8205aa00e57c5e00693a9799bf4eb40d
enzoeferreira <enzoeferreira@gmail.com>
2022-09-02 13:20:59 -0300

    [ADDED] comentários

--- math.h/trigonometria.c
+++ math.h/trigonometria.c
@@ -1,8 +1,11 @@
+// Inclusão de Bibliotecas
 #include <stdio.h>
 #include <math.h>
 
+// Definição da constante pi
 #define pi 3.141592
 
+// Funções para Secante, Cossecante e Cotangente
 double cossec(double ang)
 {
     return (1 / sin(ang));
@@ -18,12 +21,14 @@ double sec(double ang)
     return (1 / cos(ang));
 }
 
+// Função para cálculo das funções trigonométricas dependendo da unidade do angulo (rad ou graus)
 void trigonometria(char unidade)
 {
     double rad, graus, ang;
     int cosIgual0 = 0;
     int senIgual0 = 0;
     
+    // Caso Usuário queira em pi radianos
     if (unidade == 'R' || unidade == 'r')
     {
         printf("Digite a medida em pi radianos (0 a 2): ");
@@ -31,6 +36,7 @@ void trigonometria(char unidade)
         
         ang = rad * pi;
     }
+        // Caso Usuário queira em graus
         else if (unidade == 'G' || unidade == 'g')
         {
             printf("Digite a medida em graus (0 a 360): ");
@@ -39,18 +45,21 @@ void trigonometria(char unidade)
             ang = graus * (pi / 180);
         }
 
+    // pi/2, 3pi/2, 90° e 270°: cos(x) = 1
     if (rad == 0.5 || rad == 1.5 || graus == 90 || graus == 270)
     {
         cosIgual0 = 1;
     }
+    // 0pi, pi, 0° e 180°: sen(x) = 1
     if (rad == 0.0 || rad == 1.0 || graus == 0 || graus == 180)
     {
         senIgual0 = 1;
     }
 
+    // Sen e Cos estão definidas em todo dominio
     printf("Sen = %.3lf\n", sin(ang));
     printf("Cos = %.3lf\n", cos(ang));
-    if (cosIgual0)
+    if (cosIgual0) // Tg (sen/cos) e Sec (1/cos) são 1/0 -> Indeterminação
     {
         printf("Tg = Nao Existe\n");
         
@@ -58,7 +67,7 @@ void trigonometria(char unidade)
         printf("Sec = Nao Existe\n");
         printf("Cotg = %.3lf\n", cotan(ang));
     }
-        else if (senIgual0)
+        else if (senIgual0) // Cotg (cos/sen) e Cossec (1/sen) são 1/0 -> Indeterminação
         {
             printf("Tg = %.3lf\n", tan(ang));
 
@@ -66,7 +75,7 @@ void trigonometria(char unidade)
             printf("Sec = %.3lf\n", sec(ang));
             printf("Cotg = Nao Existe\n");
         }
-            else
+            else    // Todas funções trigonométricas estão definidas nesse domínio
             {
                 printf("Tg = %.3lf\n", tan(ang));
 
